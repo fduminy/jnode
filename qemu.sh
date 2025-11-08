32de282eec6f51622689a5c60e30cb1f346b66c7
fabien.duminy <fabien.duminy@webmails.com>
2025-11-08 17:46:59 +0100

    Fix qemu.sh

--- qemu.sh
+++ qemu.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-qemu-system-x86_64 -machine accel=kvm:tcg -m 768 -sdl -name "JNode x86" -cdrom all/build/cdroms/jnode-x86-lite.iso -usb -vga vmware $JNODE_QEMU_ARGS "$@"
+qemu-system-x86_64 -serial file:logs.txt -machine accel=kvm:tcg -m 768 -display sdl -name "JNode x86" -cdrom all/build/cdroms/jnode-x86-lite.iso -usb -vga vmware $JNODE_QEMU_ARGS "$@"
 
